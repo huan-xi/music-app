2435a35f5c0c889ced9d4097be4485f9b01c89d2
huangjiawei <1355473748@qq.com>
2020-07-30 21:33:39 +0800

    配置修改

--- src/common/util/config.js
+++ src/common/util/config.js
@@ -1,11 +1,6 @@
 // 域名
-const baseUrl = 'https://music.chengfeng8.top/api';
-// const baseUrl = "/api";
-
-const callbackUrl = 'https://tb.xqcrm.com/api/open/auth'
-const authUrl = `https://oauth.taobao.com/authorize?response_type=code&client_id=12686282&redirect_uri=${callbackUrl}&view=web&state=short`
-
+// const baseUrl = 'https://music.chengfeng8.top/api';
+const baseUrl = "/api";
 export default {
     baseUrl: baseUrl,
-    authUrl
 }
